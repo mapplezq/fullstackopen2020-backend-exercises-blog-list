410f85369a6ad4465b8a06dec60c53d5fa19a0d9
Mapple <mapplezq@gmail.com>
2020-08-15 15:37:36 +0800

    Blog list backend step2

--- index.js
+++ index.js
@@ -1,62 +1,10 @@
-// const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const server = http.createServer(app)
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb+srv://fullstack:zq397807729@cluster0-88lk9.mongodb.net/blog-app?retryWrites=true&w=majority'
-
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    // logger.info('connected to MongoDB')
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    // logger.error('error connection to MongoDB:', error.message)
-    console.log('error connection to MongoDB:', error.message)
-  })
-
-app.use(cors())
-app.use(express.json())
-
-const requestLogger = (request, _response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-app.use(requestLogger)
-
-app.get('/api/blogs', (req, res) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      res.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (req, res) => {
-  const blog = new Blog(req.body)
-
-  blog
-    .save()
-    .then(result => {
-      res.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
---
+++ utils/config.js
@@ -0,0 +1,8 @@
+
+let PORT = 3003
+let MONGODB_URI = 'mongodb+srv://fullstack:zq397807729@cluster0-88lk9.mongodb.net/blog-app?retryWrites=true&w=majority'
+
+module.exports = {
+  MONGODB_URI,
+  PORT
+}
